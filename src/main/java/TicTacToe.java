bffa6d6d5607ab671926b299a974b378f138e2b3
Naitik Kankariya <naitikj8200@gmail.com>
2026-05-07 16:00:35 +0530

    Added UC2 Toss Feature

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,21 +1,36 @@
+import java.util.Random;
+
 public class TicTacToe {
+
+    static String player1 = "Player 1";
+    static String player2 = "Player 2";
+
+    static char player1Symbol;
+    static char player2Symbol;
+    static String currentPlayer;
+
     public static void main(String[] args) {
 
-        char[][] board = new char[3][3];
+        // UC2: Toss to Decide First Player and Symbol
+        Random random = new Random();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            currentPlayer = player1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = player2;
+            player2Symbol = 'X';
+            player1Symbol = 'O';
         }
 
-        System.out.println("Empty Tic-Tac-Toe Board:");
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // Display Results
+        System.out.println("=== Toss Result ===");
+        System.out.println(currentPlayer + " won the toss!");
+        System.out.println(player1 + " Symbol: " + player1Symbol);
+        System.out.println(player2 + " Symbol: " + player2Symbol);
+        System.out.println(currentPlayer + " will play first.");
     }
 }
