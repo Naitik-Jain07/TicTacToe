037821771f9b1dda3cf8962bddf7078f7ef5295b
Naitik Kankariya <naitikj8200@gmail.com>
2026-05-07 16:43:40 +0530

    Implemented UC8: Continuous Turn-Based Game Loop

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class TicTacToe {
 
     private char[][] board;
+    private char currentPlayer;
     private Random random;
 
     public TicTacToe() {
         board = new char[3][3];
         random = new Random();
+        currentPlayer = 'X';
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -16,9 +19,18 @@ public class TicTacToe {
         }
     }
 
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(board[i][0] + " | " +
+                    board[i][1] + " | " +
+                    board[i][2]);
+            if (i < 2) System.out.println("--+---+--");
+        }
+    }
+
     public boolean isValidMove(int row, int col) {
-        return row >= 0 && row <= 2 &&
-                col >= 0 && col <= 2 &&
+        return row >= 0 && row < 3 &&
+                col >= 0 && col < 3 &&
                 board[row][col] == ' ';
     }
 
@@ -26,9 +38,58 @@ public class TicTacToe {
         board[row][col] = symbol;
     }
 
+    public boolean isDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer &&
+                    board[i][1] == currentPlayer &&
+                    board[i][2] == currentPlayer) return true;
+
+            if (board[0][i] == currentPlayer &&
+                    board[1][i] == currentPlayer &&
+                    board[2][i] == currentPlayer) return true;
+        }
+
+        if (board[0][0] == currentPlayer &&
+                board[1][1] == currentPlayer &&
+                board[2][2] == currentPlayer) return true;
+
+        if (board[0][2] == currentPlayer &&
+                board[1][1] == currentPlayer &&
+                board[2][0] == currentPlayer) return true;
+
+        return false;
+    }
+
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public void playerMove(Scanner sc) {
+        int slot, row, col;
+
+        do {
+            System.out.println("Player " + currentPlayer + " enter slot (1-9): ");
+            slot = sc.nextInt();
+
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+        } while (!isValidMove(row, col));
+
+        placeMove(row, col, currentPlayer);
+    }
+
     public void computerMove() {
-        int slot;
-        int row, col;
+        int slot, row, col;
 
         do {
             slot = random.nextInt(9) + 1;
@@ -39,6 +100,40 @@ public class TicTacToe {
         } while (!isValidMove(row, col));
 
         placeMove(row, col, 'O');
-        System.out.println("Computer placed at slot: " + slot);
+        System.out.println("Computer played slot: " + slot);
+    }
+
+    public void playGame() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            printBoard();
+
+            if (currentPlayer == 'X') {
+                playerMove(sc);
+            } else {
+                computerMove();
+            }
+
+            if (checkWin()) {
+                printBoard();
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            }
+
+            if (isDraw()) {
+                printBoard();
+                System.out.println("Game Draw!");
+                break;
+            }
+
+            switchPlayer();
+        }
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.playGame();
     }
 }
